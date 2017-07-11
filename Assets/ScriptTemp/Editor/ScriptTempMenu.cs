0647f1e6bafa098362b21f316ed52fd984d0c407
zouhangte <zouhangtezbm@126.com>
2017-07-11 20:56:37 +0800

    add ugui script Temp,and editor script temp

---
+++ Assets/ScriptTemp/Editor/ScriptTempMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using UnityEditor.ProjectWindowCallback;
+using System.Text.RegularExpressions;
+using System;
+using System.Text;
+
+public class ScriptTempMenu : MonoBehaviour
+{
+    [MenuItem(ScriptTempUtility.Menu_CreateUIScript, false, 80)]
+    public static void CreateUGUIScript()
+    {
+        ScriptTempUtility.CreateScriptUtil("NewUIScript.cs", ScriptTempUtility.UIScriptPath);
+    }
+
+    [MenuItem(ScriptTempUtility.Menu_CreateEditorScript, false, 70)]
+    public static void CreateEventCS()
+    {
+        ScriptTempUtility.CreateScriptUtil("NewEditorScript.cs", ScriptTempUtility.EditorScriptPath);
+    }
+
+}
---
+++ Assets/ScriptTemp/Editor/ScriptTempUtility.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using UnityEditor.ProjectWindowCallback;
+using System.Text.RegularExpressions;
+using System;
+using System.Text;
+using System.Reflection;
+
+public class ScriptTempUtility
+{
+    public static string UIScriptPath
+    {
+        get
+        {
+            var path = AssetDatabase.GUIDToAssetPath("3b09288f886811b479d032de134ad0be");
+            return path;
+        }
+    }
+    public static string EditorScriptPath
+    {
+        get
+        {
+            var path = AssetDatabase.GUIDToAssetPath("186573cc883d37a4faac6d460da92f20");
+            return path;
+        }
+    }
+
+    public const string Menu_CreateUIScript = "Assets/Create/User Script/UGUI";
+    public const string Menu_CreateEditorScript = "Assets/Create/User Script/Editor";
+    public static void CreateScriptUtil(string path, string templete)
+    {
+        MethodInfo method = typeof(ProjectWindowUtil).GetMethod("CreateScriptAsset",
+            BindingFlags.Static | BindingFlags.NonPublic);
+        if (method != null)
+            method.Invoke(null, new object[] { templete, path });
+    }
+}
+
